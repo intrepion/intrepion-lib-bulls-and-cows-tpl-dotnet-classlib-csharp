5f62d2e82f54ea94417ffc87dfc8d265b810ee66
Oliver Forral <intrepion@gmail.com>
2024-02-18 15:56:15 -0700

    red - MakeGuess

---
+++ Intrepion.BullsAndCows.Testing/MakeGuess.cs
@@ -0,0 +1,18 @@
+﻿using Intrepion.BullsAndCows.Library;
+
+namespace Intrepion.BullsAndCows.Testing;
+
+public class MakeGuess
+{
+    [Fact]
+    public void New1IntApp_DoNothingGuess_ThrowsNotReadyForGuessesException()
+    {
+        var app = new App<int>();
+        var guess = new List<int>
+        {
+            1,
+        };
+
+        Assert.Throws<NotReadyForGuessesException>(() => app.MakeGuess(guess));
+    }
+}
