8b60f1985f0ce7f8cc5ad75e7e8e0e19c4f53388
Oliver Forral <intrepion@gmail.com>
2024-02-18 13:26:40 -0700

    red - SetSecret NewIntApp_SetCharSecret_ThrowsSecretTypeMismatchException

---
+++ Intrepion.BullsAndCows.Testing/SetSecret.cs
@@ -0,0 +1,21 @@
+﻿using Intrepion.BullsAndCows.Library;
+
+namespace Intrepion.BullsAndCows.Testing;
+
+public class SetSecret
+{
+    [Fact]
+    public void NewIntApp_SetCharSecret_ThrowsSecretTypeMismatchException()
+    {
+        var app = new App<int>();
+        var secret = new List<char>
+        {
+            'a',
+            'b',
+            'c',
+            'd',
+        };
+
+        Assert.Throws<SecretTypeMismatchException>(() => app.SetSecret(secret));
+    }
+}
