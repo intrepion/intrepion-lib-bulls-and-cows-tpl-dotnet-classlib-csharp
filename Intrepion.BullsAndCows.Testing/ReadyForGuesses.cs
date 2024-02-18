04cc79c8bd1f614eafafc24c131a8090e6064938
Oliver Forral <intrepion@gmail.com>
2024-02-18 14:25:07 -0700

    red - ReadyForGuesses

--- Intrepion.BullsAndCows.Testing/ReadyForGuesses.cs
+++ Intrepion.BullsAndCows.Testing/ReadyForGuesses.cs
@@ -14,4 +14,21 @@ public class ReadyForGuesses
 
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public void New1IntApp_Set1IntSecret_ReturnTrue()
+    {
+        var expected = true;
+        var app = new App<int>();
+        app.SetSize(1);
+        var secret = new List<int>
+        {
+            1,
+        };
+        app.SetSecret(secret);
+
+        var actual = app.IsReadyForGuesses();
+
+        Assert.Equal(expected, actual);
+    }
 }
