dbbadfa19a456f8d854d7097ff5ff428eb2761e9
Oliver Forral <intrepion@gmail.com>
2024-02-18 12:25:47 -0700

    red - ReadyForGuesses

---
+++ Intrepion.BullsAndCows.Testing/ReadyForGuesses.cs
@@ -0,0 +1,17 @@
+﻿using Intrepion.BullsAndCows.Library;
+
+namespace Intrepion.BullsAndCows.Testing;
+
+public class ReadyForGuesses
+{
+    [Fact]
+    public void NewApp_DoNothing_ReturnFalse()
+    {
+        var expected = false;
+        var app = new App();
+
+        var actual = app.IsReadyForGuesses();
+
+        Assert.Equal(expected, actual);
+    }
+}
