9232b5e21ab78450cdc884cf83e9818029123693
Oliver Forral <intrepion@gmail.com>
2024-02-20 19:24:23 -0700

    red - IsDoneWithGuesses

---
+++ Intrepion.BullsAndCows.Testing/IsDoneWithGuesses.cs
@@ -0,0 +1,17 @@
+﻿using Intrepion.BullsAndCows.Library;
+
+namespace Intrepion.BullsAndCows.Testing;
+
+public class DoneWithGuesses
+{
+    [Fact]
+    public void New1IntApp_DoNothing_ReturnFalse()
+    {
+        var expected = false;
+        var app = new App<int>();
+
+        var actual = app.IsDoneWithGuesses();
+
+        Assert.Equal(expected, actual);
+    }
+}
