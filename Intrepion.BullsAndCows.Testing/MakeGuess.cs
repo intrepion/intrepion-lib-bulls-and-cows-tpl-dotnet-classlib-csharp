4814e11212f3e92b15b1042353090fcf368dd27e
Oliver Forral <intrepion@gmail.com>
2024-02-21 00:10:30 -0700

    red - MakeGuess

--- Intrepion.BullsAndCows.Testing/MakeGuess.cs
+++ Intrepion.BullsAndCows.Testing/MakeGuess.cs
@@ -34,4 +34,28 @@ public class MakeGuess
 
         Assert.Throws<GuessSizeMismatchException>(() => app.MakeGuess(guess));
     }
+
+    [Fact]
+    public void New1IntApp_GuessCorrectly_Return1Bull0Cows()
+    {
+        var expected = new int[] {
+            1,
+            0,
+        };
+        var app = new App<int>();
+        app.SetSize(1);
+        var secret = new List<int>
+        {
+            1,
+        };
+        app.SetSecret(secret);
+        var guess = new List<int>
+        {
+            1,
+        };
+
+        var actual = app.MakeGuess(guess);
+
+        Assert.Equal(expected, actual);
+    }
 }
