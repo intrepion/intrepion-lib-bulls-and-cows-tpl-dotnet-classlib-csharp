832b52f0442eb96eea98d977c8b0a5eaba529081
Oliver Forral <intrepion@gmail.com>
2024-02-20 20:51:10 -0700

    red - IsDoneWithGuesses

--- Intrepion.BullsAndCows.Testing/IsDoneWithGuesses.cs
+++ Intrepion.BullsAndCows.Testing/IsDoneWithGuesses.cs
@@ -14,4 +14,26 @@ public class DoneWithGuesses
 
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public void New1IntApp_GuessCorrectly_ReturnTrue()
+    {
+        var expected = true;
+        var app = new App<int>();
+        app.SetSize(1);
+        var secret = new List<int>
+        {
+            1,
+        };
+        app.SetSecret(secret);
+        var guess = new List<int>
+        {
+            1,
+        };
+
+        app.MakeGuess(guess);
+        var actual = app.IsDoneWithGuesses();
+
+        Assert.Equal(expected, actual);
+    }
 }
